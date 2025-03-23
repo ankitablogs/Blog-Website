f1fc9e0ba2549373151d7a4a8c51b6aec325f065
nishantgond <ankitapramanickblogs@gmail.com>
2025-03-24 02:06:10 +0530

    Update Cookie

--- backend/Controller/profileController.js
+++ backend/Controller/profileController.js
@@ -43,11 +43,11 @@ try {
 
         // Set token as a cookie
         res.cookie('token', token, {
-            httpOnly: false, // Make the cookie accessible via JavaScript
-            secure: false,   // Set to true if using HTTPS
-            sameSite: 'Strict', // Allow cross-site requests (for testing)
+            httpOnly: true, // Make the cookie accessible via JavaScript
+            secure: true,   // Set to true if using HTTPS
+            sameSite: 'none', // Allow cross-site requests (for testing)
             maxAge: 1000 * 60 * 60 * 24,
-            path: '/',
+            domain: '.vercel.app',
           });
 
         res.json({
